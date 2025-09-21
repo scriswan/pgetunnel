975622522a62bf3bce77a52d7a6441eb86dc5209
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2025-09-21 22:18:29 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -381,10 +381,10 @@ case $opt in
 7) clear ; menu-backup ;;
 8) clear ; add-host ;;
 9) clear ; running ;;
-10) clear ; wget https://raw.githubusercontent.com/gemilangvip/autoscript/main/files/udp-custom.sh && chmod +x udp-custom.sh && ./udp-custom.sh  ;;
+10) clear ; wget -q https://raw.githubusercontent.com/Riswan481/vpnstore/main/udepe.sh && chmod +x udepe.sh && ./udepe.sh && rm -f udepe.sh ;;
 11) clear ; menu-bot ;;
 12) clear ; bw ;;
-13) clear ; bash <(curl -sL https://raw.githubusercontent.com/scriswan/pgetunnel/main/install.sellvpn.sh)  ;;
+13) clear ; bash <(curl -sL https://raw.githubusercontent.com/scriswan/pgetunnel/main/install.sellvpn.sh) ;;
 14) clear ; update ;;
 0) clear ; menu ;;
 x) exit ;;
